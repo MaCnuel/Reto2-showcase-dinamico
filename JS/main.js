e56367950c510c1e0c253ea67c26f6fa5dcf15d9
Manuel Penya <manuel.penya.trabajo@gmail.com>
2020-12-28 16:42:45 +0100

    Cart List added. Lack of SHOP NOW button functionality

--- JS/main.js
+++ JS/main.js
@@ -29,6 +29,17 @@ const milkClass = new Product(Products[3,0],Products[3,1], Products[3,2], Produc
 /*****************FUNCTIONS**********************/
 /************************************************/
 
+/***************General Functions****************/
+
+
+String.prototype.removeCharAt = function () { 
+    var tmp = this.split(''); // convert to an array
+    tmp.splice(- 1 , 1); // remove 1 element from the array (adjusting for non-zero-indexed counts)
+    return tmp.join(''); // reconstruct the string
+}
+
+/************************************************/
+
 /***************Filling the market***************/
 
 /* As a repetitive stuff on the html document, we choose to fill the market in here*/
@@ -63,15 +74,14 @@ function allowDrop(ev) {
 }
   
 function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
+    ev.dataTransfer.setData("text",ev.target.id);
 }
   
 function drop(ev) {
-    ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data))
 
-    dragAndDropElement(data,ev.target.id);
+    dragAndDropElement(data);
 }
 
 /************************************************/
@@ -81,7 +91,7 @@ function drop(ev) {
 let cleanProcess = () => {
     document.querySelector('#innerTroller').innerHTML="";
     return new Promise(function (resolve, reject){
-        resolve(`${alll} Troller empty`);
+        resolve(`Troller empty`);
         reject('Error');
     })
 }
@@ -89,6 +99,7 @@ let cleanProcess = () => {
 async function cleanTroller() {
     renderBoxes(Products);
     var cleaning = await cleanProcess();
+    document.querySelector('#pricesTable').innerHTML="";
     console.log(cleaning);
 }
 
@@ -96,28 +107,79 @@ async function cleanTroller() {
 
 /******************Prices Functions*************/
 
-let countElements = (element) => {
+let priceFunction = (product) => { //Function to get the price of the product (multiplying the single product value (Products[i,2]) with the quantity of each product on the cart) 
+    for (const prod of Products){
+        switch (prod[0]){
+            case product[0]:
+                return prod[2]*product[1];
+        }
 
-    /*Generating the product table of the inner products in your troller*/
-    const productTable =[];
-    for(const product of Products){
-        var productQtyInTroller = document.getElementById('innerTroller').getElementsByClassName(`${product[0]}s`).length; 
-        //Qty of Product "i" in innerTroller
-        productTable.push([product[0],productQtyInTroller]); //Table of Products inside the troller
     }
-    return new Promise ((res, rej) => {
-        res(`A ${element} added to your cart`); 
-    })
+}
+async function FillCartHTML(product) {
+    let price = priceFunction(product); //First we calculate the price 
+    return `
+        <tr>
+            <th>${product[0]}</th>
+            <th>${product[1]}</th>
+            <th>${price} €</th>
+    `
 }
 
-async function dragAndDropElement (element, container){
-    switch (container){
-        case 'innerTroller':
-            try {
-                var newHTML = await countElements(element);
-                alert (newHTML);
-            } catch (error) {
-                return error;
-            }
+async function TotalQuantity(productTable){ //function to get the total price of the whole cart
+
+    let totalPrice = 0;
+    for (const product of productTable){
+        totalPrice += priceFunction(product)
+    } 
+    return totalPrice;
+}
+
+async function FillCart(productTable){
+
+    const pricesTable = document.querySelector('#pricesTable')
+    pricesTable.innerHTML=`<tr>
+                    <th>Product</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                </tr>` //initialicing table
+
+    for(const product of productTable){//FILLING THE BODY
+        pricesTable.innerHTML += await FillCartHTML(product);
+    }
+
+    let totalPrice = await TotalQuantity(productTable);//adding the last line with total cost
+    pricesTable.innerHTML += `<tr>
+                                <th>TOTAL</th>
+                                <th></th>
+                                <th>${totalPrice} €</th>
+                            </tr>`;
+}
+
+async function countElements(element) {
+
+    /*Generating the product table of the inner products in your troller*/
+    const productTable = [];
+    for (const product of Products) {
+        var productQtyInTroller = document.getElementById('innerTroller').getElementsByClassName(`${product[0]}s`).length;
+        //Qty of Product "i" in innerTroller
+        productTable.push([product[0], productQtyInTroller]); //Table of Products inside the troller
+    }
+    console.log(productTable);
+    /*Sending productTable to the HTML generator*/
+    FillCart(productTable);
+
+    return new Promise((res, rej) => {
+        res(`A ${element} added to your cart`);
+    });
+}
+
+async function dragAndDropElement (element){
+//    element = element.removeCharAt(); //removing the last character from the fruit id (i.e. the number)
+
+    try {
+        await countElements(element); 
+    } catch (error) {
+        console.error(error);
     }
 }
